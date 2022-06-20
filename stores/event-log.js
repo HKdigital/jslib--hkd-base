6baf26d44cc3eb951217c52fc94cf084e837dcb0
Jens Kleinhout <contact@hkdigital.nl>
2022-06-20 09:00:34 +0200

    Added store `event-log`

---
+++ stores/event-log.js
@@ -0,0 +1,25 @@
+
+import { generateLocalId } from "@hkd-base/unique.js";
+
+import ValueStore from "@hkd-base/classes/ValueStore.js";
+
+/* ------------------------------------------------------------------ Exports */
+
+export let caseId = new ValueStore( generateLocalId() );
+
+// -----------------------------------------------------------------------------
+
+/**
+ * Set a new value in the caseId store
+ *
+ * @param {string} [customCaseId]
+ *   If not set a (locally) unique caseId will be generated
+ *
+ * @returns {string} caseId
+ */
+export function setNewCaseId( customCaseId )
+{
+  caseId.set( customCaseId || generateLocalId() );
+
+  return caseId.get();
+}
