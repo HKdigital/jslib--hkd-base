d4e88e6cdfff4710d5ba9f44ee72a92f45d1d21e
Jens Kleinhout <contact@hkdigital.nl>
2024-02-12 13:19:30 -0300

    Added mime application/xml

--- constants/mime-types.js
+++ constants/mime-types.js
@@ -3,6 +3,7 @@ export const TEXT_PLAIN = "text/plain";
 export const TEXT_HTML = "text/html";
 
 export const APPLICATION_JSON = "application/json";
+export const APPLICATION_XML = "application/xml";
 
 export const IMAGE_PNG = "image/png";
 export const IMAGE_JPEG = "image/jpeg";
